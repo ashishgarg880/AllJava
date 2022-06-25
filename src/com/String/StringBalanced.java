fd2c078f52195a2d25a231e2c92766e34de58cfe
Ashish <ashishgarg880@gmail.com>
2022-06-25 11:20:38 +0530

    Success

--- src/com/String/StringBalanced.java
+++ src/com/String/StringBalanced.java
@@ -7,43 +7,40 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class StringBalanced {
-    static void Solve(LinkedList<String> list){
+    static LinkedList<Integer> Solve(LinkedList<String> list) {
         LinkedList<Integer> link = new LinkedList<>();
-        int count=0;
-        for(String str: list)
-        {
+        int count = 0;
+        for (String str : list) {
             Stack<Character> stk = new Stack<>();
-            for(int i=0;i<str.length();i++){
-                System.out.println(stk);
-                System.out.println(str.charAt(i));
-                if(stk.isEmpty()){
+            for (int i = 0; i < str.length(); i++) {
+//                System.out.println(stk);
+//                System.out.println(str.charAt(i));
+                if (stk.isEmpty()) {
                     count = 0;
                 }
-                if(str.charAt(i) == stk.peek()){
-                    if(stk.contains(str.charAt(i))) {
-                        System.out.println(" " + str.charAt(i));
-                        System.out.println("After" + stk);
-                        stk.pop();
-                    }
+                if (stk.contains(str.charAt(i))) {
+//                    System.out.println(" " + str.charAt(i));
+//                    System.out.println("After" + stk);
+                    stk.pop();
+                    count++;
+                } else {
+//                    System.out.println(" " + str.charAt(i));
+                    stk.push(str.charAt(i));
                 }
-                else{
-                        System.out.println(" "+ str.charAt(i));
-                        stk.push(str.charAt(i));
-                    }
-                }
-                if(stk.size()>0){
-                    link.add(1);
-                }else{
-                    link.add(0);
-                }
-            System.out.println("Last:::"+stk);
+            }
+            if (count%2==1) {
+                link.add(1);
+            } else {
+                link.add(0);
             }
         }
+        return link;
+    }
     public static void main(String[] args) {
         LinkedList<String> list = new LinkedList<>();
         list.add("add");
         list.add("boook");
         list.add("break");
-        Solve(list);
+        System.out.println(Solve(list));
     }
 }
